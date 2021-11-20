5506b946a50ef728586279fd1342758c01874e61
Dennis Concepcion Martin <dennisconcepcionmartin@gmail.com>
2021-11-20 18:04:06 +0100

    add setup.cfg

--- setup.py
+++ setup.py
@@ -1,24 +1,4 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 
-setup(
-    name='fucking-black-scholes',
-    description='A simple command line tool for pricing options using the Black-Scholes model',
-    version='0.0.1',
-    packages=find_packages(),
-    install_requires=['numpy~=1.21.4', 'scipy~=1.7.2', 'click~=8.0.3'],
-    python_requires='>=3.8',
-    entry_points='''
-        [console_scripts]
-        fbs=fbs.main:cli
-    ''',
-    author="Dennis Concepción Martín",
-    keyword="finance, black-scholes, option, pricing, derivative",
-    license='MIT',
-    url='https://github.com/denniscm190/fucking-black-scholes',
-    author_email='dennisconcepcionmartin@gmail.com',
-    classifiers=[
-        "License :: OSI Approved :: MIT License",
-        "Programming Language :: Python :: 3.8",
-    ]
-)
+setup()
--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -1,5 +1,5 @@
 from unittest import TestCase
-from fbs.helpers import Option
+from src.fbs.helpers import Option
 
 
 class TestOption(TestCase):
